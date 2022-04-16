0688d9b2f773fdb57ba0c1c98c21b8aee46b8dd6
karen <karenwli@seas.upenn.edu>
2022-04-15 17:27:29 -0700

    'first

--- exchange_endpoint.py
+++ exchange_endpoint.py
@@ -122,8 +122,8 @@ def get_eth_keys(filename="eth_mnemonic.txt"):
     # TODO: Generate or read (using the mnemonic secret)
     # the ethereum public/private keys
     w3.eth.account.enable_unaudited_hdwallet_features()
-    # acct, mnemonic_secret = w3.eth.account.create_with_mnemonic()
-    acct = w3.eth.account.from_mnemonic("exclude shop before cheap forward gadget loop route skin trash absent feed alien cluster federal regular mix mixed result soon mixed radio cage abstract try")
+    acct, mnemonic_secret = w3.eth.account.create_with_mnemonic()
+    acct = w3.eth.account.from_mnemonic(mnemonic_secret)
     eth_pk = acct._address
     eth_sk = acct._private_key
 
